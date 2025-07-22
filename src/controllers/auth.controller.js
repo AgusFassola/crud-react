994b9b6b616f7b2a6d78e1cdf185e8c11bbe26b8
AgusFassola <agusfassolacarp@gmail.com>
2025-07-21 23:57:13 -0300

    encriptando password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,15 +4,26 @@ import bcrypt from "bcryptjs";
 export const register = async (req, res) =>{
     const { email, password, username } = req.body;
     try{
+
+        //encriptar contraseña
+        const passwordHash = await bcrypt.hash(password, 10) 
+
         //creando el objeto usuario
         const newUser = new User({
             username,
             email,
-            password
-        })
+            password: passwordHash,
+        });
     
         const userSaved = await newUser.save();
-        res.json(userSaved)
+        res.json({
+            //eligiendo que datos mostrar
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
+        })
         res.send("registrando")
 
     }catch(error){
